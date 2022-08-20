fddd9df935654632a3e42dc5ec364e7ff6db3166
Gayaz ahamed shaik <gahamed@tracfone.com>
2022-08-20 01:38:24 -0400

    first commit

---
+++ src/main/java/com/example/demo/Rest1.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest1 {
+	
+	
+	@GetMapping("/hello")
+	public String getName() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/hello2")
+	public List<String> helloUser2() {
+		List<String> ll = new ArrayList<String>();
+		ll.add("user1");
+		ll.add("user2");
+		ll.add("user3");
+		ll.add("user4");
+		ll.add("user5");
+		ll.add("user6");		
+		return ll;
+	}
+
+	@GetMapping("/hello/{user}")
+	public String helloUser(@PathVariable String user) {
+		return "Hello World  : "+user;
+	}
+
+}
---
+++ src/main/java/com/example/demo/UserRest.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepo;
+
+@RestController
+
+public class UserRest {
+	
+	@Autowired
+	private UserRepo userrepo;
+	
+	@Value("${myprop}")
+	private String myProp;
+	
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		System.out.print("myProp :"+myProp);
+		return userrepo.findAll();
+	}
+	
+	@GetMapping("/users2")
+	public ResponseEntity getUsers2() {
+		//return userrepo.findAll();
+		return  ResponseEntity.ok(userrepo.findAll());
+	}
+	
+//	@GetMapping("/users/{id}")
+//	public User getUser(@PathVariable Integer id) {
+//		 Optional<User> us= userrepo.findById(id);
+//		 return us.get();
+//	}
+
+	
+	@GetMapping("/users/{id}")
+	public ResponseEntity getUser1(@PathVariable Integer id) {
+		 Optional<User> us= userrepo.findById(id);
+		 if(us.isPresent()) {
+			 
+			 return ResponseEntity.ok(us.get());
+		 }
+		 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User "+id +" not found");
+	}
+
+	
+	@PostMapping("/users")
+	public ResponseEntity createUser(User user) throws URISyntaxException{
+		user.setId(null);
+	User user2=	userrepo.save(user);
+		
+		return ResponseEntity.created(new URI(user2.getId().toString())).body(user2);
+	}
+	
+	
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity delUser1(@PathVariable Integer id) {
+		   userrepo.deleteById(id);
+		 
+		 return ResponseEntity.ok().build();
+	}
+}
