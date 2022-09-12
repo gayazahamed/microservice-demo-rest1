0f2c9f7a2631c88e1508bf5d3544c1ceb9261455
Gayaz <gayaz.ahamed@gmail.com>
2022-09-12 16:15:54 -0400

    property

--- eurekaserver1/src/main/java/com/me/eurekaserver1/eurekaserver1/Eurekaserver1Application.java
+++ eurekaserver1/src/main/java/com/me/eurekaserver1/eurekaserver1/Eurekaserver1Application.java
@@ -2,7 +2,9 @@ package com.me.eurekaserver1.eurekaserver1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Eurekaserver1Application {
 
