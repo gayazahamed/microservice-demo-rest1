08e707c6a446db924ee388ae490dad8600b43afd
Gayaz ahamed shaik <gahamed@tracfone.com>
2022-09-15 10:22:37 -0400

    update 2

--- ms-demo-rest1/src/main/resources/import.sql
+++ ms-demo-rest1/src/main/resources/import.sql
@@ -1,5 +1,8 @@
-insert into user_data (id , name , first_name , last_name,age,nick_Name) values (21,'Tim','first','second', 34, 'nick')
 
-insert into user_data (id , name , first_name , last_name,age,nick_Name) values (22,'Tim','first','second', 34, 'nick')
+create table testTable(id bigint not null, name varchar2(255))
 
-insert into user_data (id , name , first_name , last_name,age,nick_Name) values (23,'Tim','first','second', 34, 'nick')
+insert into user_data (id , name , first_name , last_name,age,nick_Name) values (201,'Tim','first','second', 34, 'nick')
+
+insert into user_data (id , name , first_name , last_name,age,nick_Name) values (202,'Tim','first','second', 34, 'nick')
+
+insert into user_data (id , name , first_name , last_name,age,nick_Name) values (203,'Tim','first','second', 34, 'nick')
